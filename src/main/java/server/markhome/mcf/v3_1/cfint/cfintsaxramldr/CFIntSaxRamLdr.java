c9d51def503d5157264b1501e883b4a2178fc214
Mark Sobkow <mark.sobkow@gmail.com>
2026-03-14 16:21:21 -0600

    2026-03014 16:20 CST Relicensed CFSec v3.1 and CFInt v3.1 under Apache V2

--- src/main/java/server/markhome/mcf/v3_1/cfint/cfintsaxramldr/CFIntSaxRamLdr.java
+++ src/main/java/server/markhome/mcf/v3_1/cfint/cfintsaxramldr/CFIntSaxRamLdr.java
@@ -9,26 +9,17 @@
  *	
  *	This file is part of Mark's Code Fractal CFInt.
  *	
- *	Mark's Code Fractal CFInt is available under dual commercial license from
- *	Mark Stephen Sobkow, or under the terms of the GNU Library General Public License,
- *	Version 3 or later.
+ *	Licensed under the Apache License, Version 2.0 (the "License");
+ *	you may not use this file except in compliance with the License.
+ *	You may obtain a copy of the License at
  *	
- *	Mark's Code Fractal CFInt is free software: you can redistribute it and/or
- *	modify it under the terms of the GNU Library General Public License as published by
- *	the Free Software Foundation, either version 3 of the License, or
- *	(at your option) any later version.
+ *	http://www.apache.org/licenses/LICENSE-2.0
  *	
- *	Mark's Code Fractal CFInt is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU General Public License for more details.
- *	
- *	You should have received a copy of the GNU Library General Public License
- *	along with Mark's Code Fractal CFInt.  If not, see <https://www.gnu.org/licenses/>.
- *	
- *	If you wish to modify and use this code without publishing your changes in order to
- *	tie it to proprietary code, please contact Mark Stephen Sobkow
- *	for a commercial license at mark.sobkow@gmail.com
+ *	Unless required by applicable law or agreed to in writing, software
+ *	distributed under the License is distributed on an "AS IS" BASIS,
+ *	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *	See the License for the specific language governing permissions and
+ *	limitations under the License.
  *	
  */
 
